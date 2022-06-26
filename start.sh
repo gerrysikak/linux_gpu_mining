567f74e37641ee95bd21b4a24482c3dbb72342f6
gerrysikak <108201154+gerrysikak@users.noreply.github.com>
2022-06-26 07:02:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  us-solo-eth.2miners.com:6060 --user 0xab163b91fd120715951b68a81f0e853b659f04a9.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 0
